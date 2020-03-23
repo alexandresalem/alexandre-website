ae68241c2e7d22e9cd0fc9e72e1ed3c9ab276214
Alexandre Salem <me@alexandresalem.com>
2020-03-23 18:36:21 -0500

    'Base'

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,8 +17,8 @@ import base.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    # path("", base.views.index, name="index"),
+    # path("", hello.views.index, name="index"),
+    path("", base.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
