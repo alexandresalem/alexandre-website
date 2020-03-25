787c28da31c04b275f173df44549840429314722
Alexandre Salem <me@alexandresalem.com>
2020-03-24 23:11:53 -0500

    'A'

--- base/urls.py
+++ base/urls.py
@@ -8,8 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index),
     path('drawnumber/', include('drawnumber.urls')),
-    # path('habitants/', include('habitants.urls')),
-    path('gamef1/', include('gamef1.urls')),
+    path('habitants/', include('habitants.urls')),
+    # path('gamef1/', include('gamef1.urls')),
     # path('babysize/', include('babysize.urls')),
 
 
