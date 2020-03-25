42747c30d00b65d576881258a9aec82ec887b3b3
Alexandre Salem <me@alexandresalem.com>
2020-03-24 22:02:48 -0500

    Big size

--- babysize/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# # Create your models here.
-# class BabyModel(models.Model):
-#     even_field = models.IntegerField(validators=[validade_even)
-#
--- countto5/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Countto5Config(AppConfig):
-    name = 'countto5'
