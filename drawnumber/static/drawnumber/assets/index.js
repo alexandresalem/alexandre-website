91baa6900220f1d6000504b00b322343a14d016a
Alexandre Salem <me@alexandresalem.com>
2020-04-03 10:56:56 -0500

    'Test'

--- drawnumber/static/drawnumber/assets/index.js
+++ drawnumber/static/drawnumber/assets/index.js
@@ -101,5 +101,104 @@ btnClear.addEventListener("click", function() {
 });
 
 btnGuess.addEventListener("click", function(){
-    submitNumber()
+   let prediction = predict()
+   buildchart(prediction);
 })
+
+
+// prediction function
+function predict() {
+
+    // get image from canvas
+    let image = ctx.getImageData(0, 0, 250, 250);
+
+    // create a new canvas and copy the image from drawing canvas
+    let new_canvas = document.createElement('canvas');
+
+    // canvas dimensions
+    new_canvas.height = 250;
+    new_canvas.width = 250;
+    new_canvas.getContext('2d').putImageData(image, 0, 0);
+
+    // new resized canvas 28x28
+    let resized_canvas = document.createElement('canvas');
+    resized_canvas.width = 28;
+    resized_canvas.height = 28;
+    resized_canvas.getContext('2d').scale(0.14, 0.14);
+    resized_canvas.getContext('2d').drawImage(new_canvas, 0, 0);
+
+    // uncomment to view the drawn characters
+    // document.body.appendChild(resized_canvas);
+
+
+    let image_data = resized_canvas.getContext('2d').getImageData(0, 0, 28, 28);
+
+    // change to monochrome
+    let monodata = [];
+    for (let i = 0, len = image_data.data.length/4; i < len; i += 1) {
+        monodata.push(image_data.data[i * 4 + 3]);
+        monodata.push(0);
+        monodata.push(0);
+        monodata.push(0);
+    }
+
+    // create imagedata for tfjs
+    let monoimgdata = new ImageData(new Uint8ClampedArray(monodata), 28, 28);
+
+    // get image from pixels of monochromatic image
+    let input = tf.browser.fromPixels(monoimgdata, 1).reshape([1, 28, 28, 1]).cast('float32').div(tf.scalar(255));
+
+    // prediction
+    console.log('Prediction started.'); // message
+    let predictions = model.predict(input).dataSync(); // prediction
+    console.log('Prediction completed.');// message
+    console.log(predictions)
+    return predictions;
+
+}
+
+function buildchart(predictions) {
+
+    var predictions_arr = [];
+    for (var i=0; i<=9; i++) {
+        predictions_arr[i] = predictions[i].toFixed(8) * 100;
+    }
+
+    let ctx2 = document.getElementById("prediction_chart").getContext('2d');
+    var prediction_chart = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            datasets: [{
+                label: '% confidence',
+                data: predictions,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
