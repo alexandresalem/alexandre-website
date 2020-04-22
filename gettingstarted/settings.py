f92b7a6a2ab7ed2f403d8daf7e5b0ff307ac10cb
Alexandre Salem <me@alexandresalem.com>
2020-04-22 02:43:11 -0500

    DEPLOY

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com']
 
 
 # Application definition
@@ -83,10 +83,21 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME": "mydb",
+        "USER" : "alexandredb",
+        "PASSWORD" : "abuabu444",
+        "HOST" : "localhost",
+        "PORT" : "",
     }
 }
 
