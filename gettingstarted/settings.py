350615c5d3815686e32270f8981b7f1a69c1ffc2
Alexandre Salem <me@alexandresalem.com>
2020-04-22 12:49:02 -0500

    'Draw'

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com']
+ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com', '127.0.0.1']
 
 
 # Application definition
@@ -83,23 +83,23 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE" : "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.postgresql_psycopg2",
-        "NAME": "mydb",
-        "USER" : "alexandredb",
-        "PASSWORD" : "abuabu444",
-        "HOST" : "localhost",
-        "PORT" : "",
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.postgresql_psycopg2",
+#         "NAME": "mydb",
+#         "USER" : "alexandredb",
+#         "PASSWORD" : "abuabu444",
+#         "HOST" : "localhost",
+#         "PORT" : "",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
