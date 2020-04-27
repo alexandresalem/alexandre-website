7f784bb58d57b4ce6352a45f8d0c3bcfd3af5ff8
Alexandre Salem <me@alexandresalem.com>
2020-04-26 22:33:44 -0500

    Teste

--- gamef1/models.py
+++ gamef1/models.py
@@ -1,28 +1,30 @@
 from django.db import models
-from django.core.files import File
-
-import urllib.request as urllib
 
 
-def get_remote_image(url):
-    result = urllib.urlretrieve(url)
-    return File(open(result[0]))
+# from django.core.files import File
+#
+# import urllib.request as urllib
+
+#
+# def get_remote_image(url):
+#     result = urllib.urlretrieve(url)
+#     return File(open(result[0]))
+#
 
 def test():
     result = 'Testando'
     return result
 
+
 # Create your models here.
 class Formula(models.Model):
     imagelink = models.CharField(max_length=100)
-    imagephoto = models.CharField(max_length=100, default='Xam')
-
+    imagephoto = models.CharField(max_length=100)
 
     def save(self, *args, **kwargs):
         # self.imagephoto = get_remote_image(self.imagelink)
         self.imagephoto = test()
         super(Formula, self).save(*args, **kwargs)  # Call the "real" save() method.
-
 
     def __str__(self):
         return self.imagelink
