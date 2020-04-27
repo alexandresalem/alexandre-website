c8c37e944efe3f51a99c3b49780ebae43c058e48
Alexandre Salem <me@alexandresalem.com>
2020-04-26 22:48:10 -0500

    Xam

--- gamef1/forms.py
+++ gamef1/forms.py
@@ -2,12 +2,25 @@ from django import forms
 from .models import Formula
 
 
+def test():
+    result = 'Xam2'
+    return result
+
+
 # Create your form
 class FormulaForm(forms.ModelForm):
+    def save(self, commit=True):
+        m = super(FormulaForm, self).save(commit=False)
+        # do custom stuff
+        self.imagephoto = test()
+        if commit:
+            m.save()
+        return m
+
     class Meta:
         model = Formula
-        fields = ['imagelink']
-
-        help_texts = {
-            'imagelink': 'Group to which this message belongs to',
-        }
+        # fields = ['imagelink']
+        #
+        # help_texts = {
+        #     'imagelink': 'Group to which this message belongs to',
+        # }
