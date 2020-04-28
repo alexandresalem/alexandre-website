a1741e357abae6d65879b791ff120bbffd3e997b
Alexandre Salem <me@alexandresalem.com>
2020-04-27 21:45:57 -0500

    ImageField

--- gamef1/models.py
+++ gamef1/models.py
@@ -1,21 +1,4 @@
 from django.db import models
-from django.core.files import File
-import urllib.request as urllib
-import os
-
-# from django.core.files import File
-#
-# import urllib.request as urllib
-
-#
-# def get_remote_image(url):
-#     result = urllib.urlretrieve(url)
-#     return File(open(result[0]))
-#
-
-# def test(url):
-#     result = f"Teeeet {url}"
-#     return result
 
 
 def get_remote_image(url):
@@ -26,13 +9,7 @@ def get_remote_image(url):
 # Create your models here.
 class Formula(models.Model):
     imagelink = models.CharField(max_length=500)
-    imagephoto = models.CharField(max_length=500)
-
-
-    def save(self, *args, **kwargs):
-        result = urllib.urlretrieve(self.imagelink)
-        self.imagephoto = result[0]
-        super(Formula, self).save(*args, **kwargs)
+    imagephoto = models.ImageField(upload_to='gamef1photos/')
 
 
 
