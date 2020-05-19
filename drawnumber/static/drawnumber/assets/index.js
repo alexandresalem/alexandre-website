834243432b013380acc4fc7aa6a15627435c79ef
Alexandre Salem <alexandresalem@salem-debian.domain-debian>
2020-05-18 21:23:11 -0500

    Index.js Touch Events

--- drawnumber/static/drawnumber/assets/index.js
+++ drawnumber/static/drawnumber/assets/index.js
@@ -40,6 +40,26 @@ function getMousePos(canvasDom, mouseEvent) {
   };
 }
 
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
+
+
+
 // Set up touch events for mobile, etc
 canvas.addEventListener("touchstart", function (e) {
         mousePos = getTouchPos(canvas, e);
@@ -83,22 +103,7 @@ function renderCanvas() {
   }
 }
 
-// Prevent scrolling when touching the canvas
-document.body.addEventListener("touchstart", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
-document.body.addEventListener("touchend", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
-document.body.addEventListener("touchmove", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
-}, false);
+
 //    photo = document.getElementById('myCanvas')
 //    ctx = photo.getContext("2d");
 //
