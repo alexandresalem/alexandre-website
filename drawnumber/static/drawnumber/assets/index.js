8c3a838ec1cf865567e6057db815d6b8a2e7ac85
Alexandre Salem <alexandresalem@salem-debian.domain-debian>
2020-05-18 21:10:36 -0500

    Draw Number Model

--- drawnumber/static/drawnumber/assets/index.js
+++ drawnumber/static/drawnumber/assets/index.js
@@ -85,17 +85,17 @@ function renderCanvas() {
 
 // Prevent scrolling when touching the canvas
 document.body.addEventListener("touchstart", function (e) {
-  if (e.target == photo) {
+  if (e.target == canvas) {
     e.preventDefault();
   }
 }, false);
 document.body.addEventListener("touchend", function (e) {
-  if (e.target == photo) {
+  if (e.target == canvas) {
     e.preventDefault();
   }
 }, false);
 document.body.addEventListener("touchmove", function (e) {
-  if (e.target == photo) {
+  if (e.target == canvas) {
     e.preventDefault();
   }
 }, false);
