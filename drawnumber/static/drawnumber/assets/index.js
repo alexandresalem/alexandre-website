d960c80b879d373fcc11b1a5804f8ed746e1f2e2
Alexandre Salem <alexandresalem@salem-debian.domain-debian>
2020-05-18 19:25:20 -0500

    Touch Events

--- drawnumber/static/drawnumber/assets/index.js
+++ drawnumber/static/drawnumber/assets/index.js
@@ -2,17 +2,107 @@
 const btnClear = document.querySelector("#btnClear");
 const btnGuess = document.querySelector("#btnGuess");
 
-let img;
-let mousePressed = false;
-let lastY;
-let lastX;
-let ctx;
-let photo;
-function initThis() {
-    photo = document.getElementById('myCanvas')
-    ctx = photo.getContext("2d");
+//let img;
+//let mousePressed = false;
+//let lastY;
+//let lastX;
+//let ctx;
+//let photo;
 
-    photo.addEventListener
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#000000";
+ctx.lineWidth = 9;
+ctx.lineJoin = "round";
+
+// Set up mouse events for drawing
+var drawing = false;
+var mousePos = { x:0, y:0 };
+var lastPos = mousePos;
+canvas.addEventListener("mousedown", function (e) {
+    drawing = true;
+    lastPos = getMousePos(canvas, e);
+    }, false);
+canvas.addEventListener("mouseup", function (e) {
+    drawing = false;
+    }, false);
+canvas.addEventListener("mousemove", function (e) {
+    mousePos = getMousePos(canvas, e);
+    renderCanvas();
+    }, false);
+
+// Get the position of the mouse relative to the canvas
+function getMousePos(canvasDom, mouseEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: mouseEvent.clientX - rect.left,
+    y: mouseEvent.clientY - rect.top
+  };
+}
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+
+
+// Draw to the canvas
+function renderCanvas() {
+  if (drawing) {
+    ctx.moveTo(lastPos.x, lastPos.y);
+    ctx.lineTo(mousePos.x, mousePos.y);
+    ctx.stroke();
+    lastPos = mousePos;
+  }
+}
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == photo) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == photo) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == photo) {
+    e.preventDefault();
+  }
+}, false);
+//    photo = document.getElementById('myCanvas')
+//    ctx = photo.getContext("2d");
+//
+//    photo.addEventListener
 //    $('#myCanvas').touchstart(function (e) {
 //        e.preventDefault();
 //        mousePressed = true;
@@ -28,46 +118,51 @@ function initThis() {
 //        e.preventDefault();
 //        mousePressed = false;
 //    });
+//
+//
+//    $('#myCanvas').mousedown(function (e) {
+//        mousePressed = true;
+//        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+//    });
+//    $('#myCanvas').mousemove(function (e) {
+//        if (mousePressed) {
+//            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+//        }
+//    });
+//    $('#myCanvas').mouseup(function (e) {
+//        mousePressed = false;
+//    });
+//    $('#myCanvas').mouseleave(function (e) {
+//        mousePressed = false;
+//    });
+//
+//}
 
-
-    $('#myCanvas').mousedown(function (e) {
-        mousePressed = true;
-        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-    });
-    $('#myCanvas').mousemove(function (e) {
-        if (mousePressed) {
-            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-        }
-    });
-    $('#myCanvas').mouseup(function (e) {
-        mousePressed = false;
-    });
-    $('#myCanvas').mouseleave(function (e) {
-        mousePressed = false;
-    });
-
-}
-initThis();
-
-function Draw(x, y, isDown) {
-    if (isDown) {
-        ctx.beginPath();
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = 9;
-        ctx.lineJoin = "round";
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(x, y);
-        ctx.closePath();
-        ctx.stroke();
-    }
-    lastX = x;
-    lastY = y;
-}
+//
+//function Draw(x, y, isDown) {
+//    if (isDown) {
+//ctx.beginPath();
+//ctx.strokeStyle = "#000000";
+//ctx.lineWidth = 9;
+//ctx.lineJoin = "round";
+//ctx.moveTo(lastX, lastY);
+//ctx.lineTo(x, y);
+//ctx.closePath();
+//ctx.stroke();
+//    }
+//    lastX = x;
+//    lastY = y;
+//}
 
 function clearArea() {
     // Use the identity matrix while clearing the canvas
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    canvas.width = canvas.width;
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 9;
+    ctx.lineJoin = "round";
+//    ctx.setTransform(1, 0, 0, 1, 0, 0);
+//    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
 
@@ -101,7 +196,7 @@ btnGuess.addEventListener("click", function(){
 function predict() {
 
     // get image as DataUrl
-    img = photo.toDataURL('image/png');
+    img = canvas.toDataURL('image/png');
     console.log(img);
 //    fillImage();
     // get image from canvas
@@ -214,8 +309,10 @@ function buildchart(predictions) {
 function fillImage(){
 //    let field = document.getElementById('drawing')
     let fieldcode = document.getElementById('drawingcode')
-//    field.value = 'Oxe'
+
     fieldcode.value = img
     console.log(img)
 }
+
+
 
--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -27,8 +27,11 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com']
-# ALLOWED_HOSTS = []
+if not os.environ.get('SSH_CLIENT'):
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com']
+
 
 
 # Application definition
@@ -84,23 +87,24 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE" : "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-#     }
-# }
-#
-DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.postgresql_psycopg2",
-        "NAME": "mydb",
-        "USER" : "alexandredb",
-        "PASSWORD" : "abuabu444",
-        "HOST" : "localhost",
-        "PORT" : "",
+if not os.environ.get('SSH_CLIENT'):
+    DATABASES = {
+        "default": {
+            "ENGINE" : "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE" : "django.db.backends.postgresql_psycopg2",
+            "NAME": "mydb",
+            "USER" : "alexandredb",
+            "PASSWORD" : "abuabu444",
+            "HOST" : "localhost",
+            "PORT" : "",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
