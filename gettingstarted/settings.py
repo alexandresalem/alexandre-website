c7643bf956bc073003006c4e40185b6247ded4c5
Alexandre Salem <alexandresalem@salem-debian.domain-debian>
2020-05-18 19:48:40 -0500

    Test

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+import ipdb; ipdb.set_trace()
 if os.environ.get('SSH_CLIENT') == None:
     ALLOWED_HOSTS = []
 else:
