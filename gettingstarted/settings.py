178426bb6bd9a5ee36003e0d9c1674315fb01d98
Alexandre Salem <salem.alexandre@gmail.com>
2020-06-17 19:59:46 -0500

    ALLOWED HOSTS

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -80,33 +80,20 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if not os.environ.get('SSH_CONNECTION'):
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
+DEBUG = False
 
-    ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com','127.0.0.1','localhost']
 
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-        }
+DATABASES = {
+    "default": {
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
+        "NAME": "mydb",
+        "USER" : "alexandredb",
+        "PASSWORD" : "abuabu444",
+        "HOST" : "localhost",
+        "PORT" : "",
     }
-else:
-    DEBUG = False
-
-    ALLOWED_HOSTS = ['45.132.242.38','alexandresalem.com']
-
-    DATABASES = {
-        "default": {
-            "ENGINE" : "django.db.backends.postgresql_psycopg2",
-            "NAME": "mydb",
-            "USER" : "alexandredb",
-            "PASSWORD" : "abuabu444",
-            "HOST" : "localhost",
-            "PORT" : "",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
