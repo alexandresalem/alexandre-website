37d39fa12ed38ea5e1384cb494eb60f82c7daf3b
Alexandre Salem <salem.alexandre@gmail.com>
2020-09-04 17:07:06 -0500

    STATIC DIR

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,30 +1,13 @@
-from django.urls import path, include
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
 admin.autodiscover()
 
-# import hello.views
-# import base.views
-
-# import habitants.views
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
-    # path("", hello.views.index, name="index"),
-    # path("", base.views.index, name="index"),
     path("", include('base.urls')),
-    # path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
-
-
 ]
 
 if settings.DEBUG:
