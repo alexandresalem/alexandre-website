ad21ef36c7f3dd73803e4b7b8b5ffb78bcc70630
Alexandre Salem <salem.alexandre@gmail.com>
2020-09-03 20:58:09 -0500

    Settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.postgresql_psycopg2",
-        "NAME": "mydb",
+        "NAME": "mydb2",
         "USER" : "alexandredb",
         "PASSWORD" : "abuabu444",
         "HOST" : "localhost",
