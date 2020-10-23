12931f1717f7415ded32ea024f9d288a9961c7d7
Alexandre Salem <salem.alexandre@gmail.com>
2020-10-22 23:00:16 -0500

    F1 Akinator - Fuction Based View

--- f1pilot/views.py
+++ f1pilot/views.py
@@ -13,48 +13,50 @@ media_storage = FileSystemStorage()
 def f1pilot(request):
 
     if request.method == "POST":
-        if 'start' in request.POST:
-            sheet = static_storage.path('f1pilot/questions.csv')
-            df = pd.read_csv(sheet)
-            with open(sheet) as file:
-                media_storage.save('f1pilot/questions.csv', file)
-            question_round, result = get_question(df)
-            context = {
-                'question_round': question_round
-            }
-            return render(request, 'f1pilot/game.html', context)
+        return render(request, 'f1pilot/game.html')
 
-        elif 'answer' in request.POST:
-            answer = request.POST['answer']
-            file = media_storage.path('f1pilot/questions.csv')
-            df = pd.read_csv(file)
-
-            if len(df) <= 1:
-                if answer == 'Yes':
-                    question_round, result = "Congratulations!", True
-
-                    return render(request, 'f1pilot/game.html', {'question_round': question_round})
-                else:
-                    media_storage.delete('f1pilot/questions.csv')
-                    sheet = static_storage.path('f1pilot/questions.csv')
-                    df = pd.read_csv(sheet)
-                    with open(sheet) as file:
-                        media_storage.save('f1pilot/questions.csv', file)
-                    question_round, result = get_question(df)
-                    return render(request, 'f1pilot/game.html', {'question_round': question_round})
-            else:
-                question_round, result = get_question(df)
-
-            if answer == 'No':
-                df = df[df[question_round] == 0].drop(columns=[question_round])
-            elif answer == 'Yes':
-                df = df[df[question_round] == 1].drop(columns=[question_round])
-            elif answer == "Don't Know":
-                df = df.drop(columns=[question_round])
-
-            df.to_csv(media_storage.path('f1pilot/questions.csv'), index=False)
-            question_round, result = get_question(df)
-
-            return render(request, 'f1pilot/game.html', {'question_round': question_round, 'result': result})
-
-    return render(request, 'f1pilot/home.html')
+        # if 'start' in request.POST:
+        #     sheet = static_storage.path('f1pilot/questions.csv')
+        #     df = pd.read_csv(sheet)
+        #     with open(sheet) as file:
+        #         media_storage.save('f1pilot/questions.csv', file)
+        #     question_round, result = get_question(df)
+        #     context = {
+        #         'question_round': question_round
+        #     }
+        #     return render(request, 'f1pilot/game.html', context)
+        #
+        # elif 'answer' in request.POST:
+        #     answer = request.POST['answer']
+        #     file = media_storage.path('f1pilot/questions.csv')
+        #     df = pd.read_csv(file)
+        #
+        #     if len(df) <= 1:
+        #         if answer == 'Yes':
+        #             question_round, result = "Congratulations!", True
+        #
+        #             return render(request, 'f1pilot/game.html', {'question_round': question_round})
+        #         else:
+        #             media_storage.delete('f1pilot/questions.csv')
+        #             sheet = static_storage.path('f1pilot/questions.csv')
+        #             df = pd.read_csv(sheet)
+        #             with open(sheet) as file:
+        #                 media_storage.save('f1pilot/questions.csv', file)
+        #             question_round, result = get_question(df)
+        #             return render(request, 'f1pilot/game.html', {'question_round': question_round})
+        #     else:
+        #         question_round, result = get_question(df)
+        #
+        #     if answer == 'No':
+        #         df = df[df[question_round] == 0].drop(columns=[question_round])
+        #     elif answer == 'Yes':
+        #         df = df[df[question_round] == 1].drop(columns=[question_round])
+        #     elif answer == "Don't Know":
+        #         df = df.drop(columns=[question_round])
+        #
+        #     df.to_csv(media_storage.path('f1pilot/questions.csv'), index=False)
+        #     question_round, result = get_question(df)
+        #
+        #     return render(request, 'f1pilot/game.html', {'question_round': question_round, 'result': result})
+    # 
+    # return render(request, 'f1pilot/home.html')
