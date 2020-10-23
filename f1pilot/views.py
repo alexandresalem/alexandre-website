a7b9e02d16cfa9fbe74bb0d0288c07462d0f251b
Alexandre Salem <salem.alexandre@gmail.com>
2020-10-22 21:06:13 -0500

    F1 Akinator - Fuction Based View

--- f1pilot/views.py
+++ f1pilot/views.py
@@ -7,16 +7,17 @@ from f1pilot.utils import get_question
 from gettingstarted.settings import STATIC_ROOT, STATIC_URL
 
 static_storage = FileSystemStorage(location=STATIC_ROOT, base_url=STATIC_URL)
-media_storage = FileSystemStorage(location=STATIC_ROOT, base_url=STATIC_URL)
+media_storage = FileSystemStorage()
 
 
 def f1pilot(request):
 
     if request.method == "POST":
         if 'start' in request.POST:
-            file = static_storage.path('f1pilot/questions.csv')
-            df = pd.read_csv(file)
-            df.to_csv(media_storage.path('f1pilot/questions.csv'), index=False)
+            sheet = static_storage.path('f1pilot/questions.csv')
+            df = pd.read_csv(sheet)
+            with open(sheet) as file:
+                media_storage.save('f1pilot/questions.csv', file)
             question_round, result = get_question(df)
             context = {
                 'question_round': question_round
@@ -31,11 +32,14 @@ def f1pilot(request):
             if len(df) <= 1:
                 if answer == 'Yes':
                     question_round, result = "Congratulations!", True
+
                     return render(request, 'f1pilot/game.html', {'question_round': question_round})
                 else:
-                    file = static_storage.path('f1pilot/questions.csv')
-                    df = pd.read_csv(file)
-                    df.to_csv(media_storage.path('f1pilot/questions.csv'), index=False)
+                    media_storage.delete('f1pilot/questions.csv')
+                    sheet = static_storage.path('f1pilot/questions.csv')
+                    df = pd.read_csv(sheet)
+                    with open(sheet) as file:
+                        media_storage.save('f1pilot/questions.csv', file)
                     question_round, result = get_question(df)
                     return render(request, 'f1pilot/game.html', {'question_round': question_round})
             else:
